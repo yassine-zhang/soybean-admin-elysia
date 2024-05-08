a2d7dd3ed507f7507001f88a6df1748bc7c44bfc
YassineZhang <57878778@qq.com>
2024-05-08 11:24:51 +0800

    feat: 配置CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia";
 import { jwt } from "@elysiajs/jwt";
+import { cors } from "@elysiajs/cors";
 
 import { authPlugin } from "../handler/auth";
 
 const jwtSecret = Bun.env.JWT_SECRET || "";
 
 new Elysia()
+  .use(cors())
   .use(
     jwt({
       name: "jwt",
